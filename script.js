a76684582dda903dbc344e3e9815ce80b9705386
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-08-13 21:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function toupper() {
+	let name=document.getElementById("fname");
+	name.value=name.value.toUpperCase();
+	console.log(name.value);
+}
+
+
+window.onload=function() {
+	document.getElementById("fname").addEventListener("blur",toupper);
+};
